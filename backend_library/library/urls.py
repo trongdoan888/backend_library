0dbbb3befc71f8a6054a5f0801c990a935071a78
Tên của bạn <trongdoan7689@gmail.com>
2026-08-06 21:48:55 +0700

    change some code

--- backend_library/library/urls.py
+++ backend_library/library/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library_service project.
+URL configuration for library project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
